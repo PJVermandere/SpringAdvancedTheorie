ae8b3ac8829849e254729d083b23f51892ba2b7a
main <prince.vermandere@outlook.be>
2021-12-03 10:05:40 +0100

    Initial commit

---
+++ src/main/java/com/example/springadvancedtheorie/SpringAdvancedTheorieApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadvancedtheorie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedTheorieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedTheorieApplication.class, args);
+    }
+
+}
