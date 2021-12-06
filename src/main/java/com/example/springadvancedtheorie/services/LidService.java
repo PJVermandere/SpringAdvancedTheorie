6b1839e6a1c4d251babea7e935c83a3275aec625
main <prince.vermandere@outlook.be>
2021-12-06 14:13:15 +0100

    added findById

--- src/main/java/com/example/springadvancedtheorie/services/LidService.java
+++ src/main/java/com/example/springadvancedtheorie/services/LidService.java
@@ -2,6 +2,9 @@ package com.example.springadvancedtheorie.services;
 
 import com.example.springadvancedtheorie.domain.Lid;
 
+import java.util.Optional;
+
 public interface LidService {
+    Optional<Lid> findById(long id);
     void nieuweInschrijving(Lid lid, String url);
 }
